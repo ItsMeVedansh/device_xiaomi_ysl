e3f48e5571e103bf337d95698e7f7cc6a5e0179f
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-06-06 20:00:18 +0530

    ysl: prop: add Android Go LMKD props

--- prop.mk
+++ prop.mk
@@ -268,3 +268,16 @@ sys.vendor.shutdown.waittime=500
 # Enable QC2 bufferqueue block-pool
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.qc2.use.bqpool=1
+
+# Zram-writeback
+ro.zram.mark_idle_delay_mins=60 
+ro.zram.first_wb_delay_mins=180
+ro.zram.periodic_wb_delay_hours=24
+
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.lmk.critical_upgrade=true \
+ro.lmk.upgrade_pressure=40 \
+ro.lmk.downgrade_pressure=60 \
+ro.lmk.thrashing_limit=30 \
+ro.lmk.thrashing_limit_decay=50
